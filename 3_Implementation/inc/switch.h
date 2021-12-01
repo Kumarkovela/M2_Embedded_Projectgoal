49c9a3f920163cd366aba07e1da125132d9231ca
Kumarkovela <94270860+Kumarkovela@users.noreply.github.com>
2021-12-01 22:02:50 +0530

    Update switch.h

--- 3_Implementation/inc/switch.h
+++ 3_Implementation/inc/switch.h
@@ -1,8 +1,8 @@
 
-#ifndef RELAY_H_INCLUDE
-#define RELAY_H_INCLUDE
+#ifndef RELAY_H_INCLUDE_
+#define RELAY_H_INCLUDE_
 
 void floatSwitchInit(void);
 
-#endif // RELAY_H_INCLUDE
+#endif // RELAY_H_INCLUDE_
 
